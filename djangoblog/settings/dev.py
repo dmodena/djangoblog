4485d3312ef9524690bca2086bd744eaf0d9908a
Douglas Modena <d.modena@live.com>
2018-02-27 16:34:44 -0300

    Test with databases config moved to base.

--- djangoblog/settings/dev.py
+++ djangoblog/settings/dev.py
@@ -5,9 +5,3 @@ from decouple import config
 DEBUG = True
 ALLOWED_HOSTS = []
 SECRET_KEY = config('SECRET_KEY')
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
